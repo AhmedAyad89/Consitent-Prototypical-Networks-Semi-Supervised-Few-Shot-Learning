fixed multi-hop softmax fixed tournament shuffling and partitioning

fewshot/models/basic_model_VAT_ENT.py
@@ -66,6 +66,7 @@ class BasicModelENT(RefineModel):
 
 	def get_train_op(self, logits, y_test):
 		loss, train_op = RefineModel.get_train_op(self, logits, y_test)
+		classification_loss = loss
 		config = self.config
 		ENT_weight = config.ENT_weight
 		ENT_step_size = config.ENT_step_size
@@ -106,20 +107,39 @@ class BasicModelENT(RefineModel):
 			# ENT_loss = walking_penalty_matching(logits_1, affinity_matrix_1, logits_2, affinity_matrix_2)
 			s = tf.shape(self._unlabel_logits)[0]
 			affinity_matrix = compute_logits(p, p) - (tf.eye(s, dtype=tf.float32) * 1000.0)
-			ENT_loss = walking_penalty_matching_tournament(self._unlabel_logits, affinity_matrix)
+			ENT_loss, rw_loss, landing_probs_list = walking_penalty_matching_tournament(self._unlabel_logits, affinity_matrix, p)
 
 		loss += ENT_weight * ENT_loss
+
 
 		ENT_opt = tf.train.AdamOptimizer(ENT_step_size * self.learn_rate, name="Entropy-optimizer")
 		ENT_grads_and_vars = ENT_opt.compute_gradients(loss)
 		train_op = ENT_opt.apply_gradients(ENT_grads_and_vars)
 
-		for gradient, variable in ENT_grads_and_vars:
+		classification_grads = ENT_opt.compute_gradients(classification_loss)
+		rw_grads = ENT_opt.compute_gradients(rw_loss)
+		count = 0
+		for gradient, variable in rw_grads:
 			if gradient is None:
 				gradient = tf.constant(0.0)
+				count+=1
 			self.adv_summaries.append(tf.summary.scalar("ENT/gradients/" + variable.name, l2_norm(gradient), family="Grads"))
 			self.adv_summaries.append(tf.summary.histogram("ENT/gradients/" + variable.name, gradient, family="Grads"))
 
-		self.summaries.append(tf.summary.scalar('entropy loss', ENT_loss))
+		self.summaries.append(tf.summary.scalar('RW loss', rw_loss))
+		self.summaries.append(tf.summary.scalar('Zero grad count', count))
+
+		self.adv_summaries.append(tf.summary.histogram('Correct landing hist zero hop', tf.diag_part(landing_probs_list[0][0]), family='landing probs'))
+		self.adv_summaries.append(tf.summary.histogram('Correct landing hist one hop', tf.diag_part(landing_probs_list[0][1]), family='landing probs'))
+		self.adv_summaries.append(tf.summary.histogram('Correct landing hist two hop', tf.diag_part(landing_probs_list[0][2]), family='landing probs'))
+
+		self.adv_summaries.append(tf.summary.histogram('all landing histo', landing_probs_list, family='landing probs'))
+		self.adv_summaries.append(tf.summary.scalar('avg correct prob zero hop', tf.reduce_mean(tf.diag_part(landing_probs_list[0][0])), family='landing probs'))
+		self.adv_summaries.append(tf.summary.scalar('avg correct prob one hop', tf.reduce_mean(tf.diag_part(landing_probs_list[0][1])), family='landing probs'))
+		self.adv_summaries.append(tf.summary.scalar('avg correct prob two hop', tf.reduce_mean(tf.diag_part(landing_probs_list[0][2])), family='landing probs'))
+
 
 		return loss, train_op
+
+	def entropy_summaries(self, total_loss, rw_loss, landing_probs_list):
+		pass